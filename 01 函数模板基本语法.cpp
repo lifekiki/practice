0ca16a58deec4bd6173867a568f225bf328cb7e3
lifekiki <45809848+lifekiki@users.noreply.github.com>
2022-01-27 11:15:42 +0800

    the first file upload

---
+++ 01 函数模板基本语法.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//using namespace std;
+//template<typename T>
+//void mySwap(T& a, T& b) {
+//	T temp = a;
+//	a = b;
+//	b = temp;
+//}
+//void test01() {
+//	int a = 10;
+//	int b = 20;
+//
+//	//mySwap(a, b);
+//	mySwap<int>(a, b);
+//	cout << "a= " << a << "b= "<<b << endl;
+//}
+//int main() {
+//	test01();
+//	system("pause");
+//	system("cls");
+//}
